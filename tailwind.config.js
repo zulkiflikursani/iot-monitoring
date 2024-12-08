e81bc82bf2605667dcde55b9211c9aa977ab5f34
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2024-12-08 13:25:58 +0800

    firts commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 import { nextui } from "@nextui-org/theme";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default module.exports = {
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -17,4 +17,7 @@ module.exports = {
   },
   darkMode: "class",
   plugins: [nextui()],
+  experimental: {
+    externalDir: true,
+  },
 };
